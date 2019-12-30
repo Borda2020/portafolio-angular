73124599510fae492393dc28656c0848f1b5bbe9
Carlos Borda <bordanonatoalonso@gmail.com>
2019-12-30 12:18:49 -0500

    servicios de firebase

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -9,14 +9,27 @@ export class InfoPaginaService {
 
   info: InfoPagina = {};
   cargada = false;
+  equipo: any[] = [];
 
   constructor( private http: HttpClient) {
-    // console.log('Servicio de infoPagina listo');
+      this.cargarInfo();
+      this.cargarEquipo();
+  }
+
+  private cargarInfo(){
+     // console.log('Servicio de infoPagina listo');
     // leer el archivo JSON
     this.http.get('assets/data/data-pagina.json')
         .subscribe( (resp: InfoPagina) => {
             this.cargada = true;
             this.info = resp;
+        });
+  }
+
+  private cargarEquipo(){
+    this.http.get('https://angular-portafolio-37b85.firebaseio.com/equipo.json')
+        .subscribe( (resp: any[]) => {
+            this.equipo = resp;
             console.log(resp);
         });
   }
