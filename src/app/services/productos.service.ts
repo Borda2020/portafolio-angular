6f71cb8459b51e83295d3d04b94322f7909c78b6
Carlos Borda <bordanonatoalonso@gmail.com>
2020-01-19 09:48:58 -0500

    listo para produccion

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -8,19 +8,47 @@ import { Producto } from '../interfaces/producto.interface';
 export class ProductosService {
   cargando = true;
   productos: Producto[] = [];
+  productosFiltrado: Producto[] = [];
   constructor( private http: HttpClient ) {
     this.cargarProductos();
   }
 
   private cargarProductos(){
+    return new Promise( ( resolve, reject ) => {
     this.http.get('https://angular-portafolio-37b85.firebaseio.com/productos_idx.json')
-        .subscribe( (resp: Producto[]) => {
-            this.productos = resp;
-            this.cargando = false;
-        });
+    .subscribe( (resp: Producto[]) => {
+        this.productos = resp;
+        this.cargando = false;
+      });
+      });
     }
     getProducto( id: string ) {
       return this.http.get(`https://angular-portafolio-37b85.firebaseio.com/productos/${ id }.json`);
       console.log(id);
     }
+    buscarProducto( termino: string ){
+      if ( this.productos.length === 0 ){
+        // cargar productos
+        this.cargarProductos().then( () => {
+        // ejecutar despues de tener los productos
+        // aplicar filtro
+        this.filtrarProductos( termino );
+        });
+      } else {
+        // aplicar el filtro
+        this.filtrarProductos( termino );
+      }
+    }
+     private filtrarProductos( termino: string) {
+        this.productosFiltrado = [] ;
+        // para evaluar los datos en minusculas
+        termino = termino.toLocaleLowerCase();
+        this.productos.forEach( prod => {
+          const tituloLower = prod.titulo.toLocaleLowerCase();
+          // condiciones de busqueda
+            if ( prod.categoria.indexOf( termino ) >= 0 || tituloLower.indexOf( termino ) >= 0 ) {
+              this.productosFiltrado.push( prod );
+            }
+        });
+     }
 }
